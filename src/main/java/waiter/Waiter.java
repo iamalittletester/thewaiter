82c809909ef527e4fef5c1cb368f457f983cd0e4
corina.pip <capreoara6@gmail.com>
2018-01-03 14:36:53 +0200

    Wait methods for: url, click-and-wait.

--- src/main/java/waiter/Waiter.java
+++ src/main/java/waiter/Waiter.java
@@ -181,4 +181,264 @@ public class Waiter {
         wait.until(urlIsCorrect);
         waitForPageLoadComplete(driver, specifiedTimeout);
     }
+
+    /**
+     * Wait for a URL containing a specified String to open in the browser.
+     * The URL will not equal the specified String. Just contain it.
+     * Wait for TIMEOUT number of seconds.
+     *
+     * @param expectedString - the String that needs to be included in the URL
+     * @param driver         - the WebDriver instance
+     */
+    public void waitForUrlContains(String expectedString, WebDriver driver) {
+        waitForUrlContains(expectedString, driver, TIMEOUT);
+    }
+
+    /**
+     * Wait for a URL containing a specified String to open in the browser.
+     * The URL will not equal the specified String. Just contain it.
+     * Wait for the specifiedTimeout number of seconds.
+     *
+     * @param expectedString   - the String that needs to be included in the URL
+     * @param driver           - the WebDriver instance
+     * @param specifiedTimeout - amount of seconds you want to wait for
+     */
+    public void waitForUrlContains(String expectedString, WebDriver driver, int specifiedTimeout) {
+        WebDriverWait wait = new WebDriverWait(driver, specifiedTimeout);
+        ExpectedCondition<Boolean> urlIsCorrect = arg0 -> driver.getCurrentUrl().contains(expectedString);
+        wait.until(urlIsCorrect);
+        waitForPageLoadComplete(driver, specifiedTimeout);
+    }
+
+
+    /**
+     * Wait for an expected URL to load in the browser, but ignore the case of the url.
+     * Compares a lower case value of the actual url in the browser with the lower case value of the expected url.
+     * Wait for the TIMEOUT number of seconds.
+     *
+     * @param url    - the url expected to load in the browser, ignoring its case
+     * @param driver - the WebDriver instance
+     */
+    public void waitForUrl_IgnoreCase(String url, WebDriver driver) {
+        waitForUrl_IgnoreCase(url, driver, TIMEOUT);
+    }
+
+    /**
+     * Wait for an expected URL to load in the browser, but ignore the case of the url.
+     * Compares a lower case value of the actual url in the browser with the lower case value of the expected url.
+     * Wait for the specifiedTimeout number of seconds.
+     *
+     * @param url              - the url expected to load in the browser, ignoring its case
+     * @param driver           - the WebDriver instance
+     * @param specifiedTimeout - amount of seconds you want to wait for
+     */
+    public void waitForUrl_IgnoreCase(String url, WebDriver driver, int specifiedTimeout) {
+        WebDriverWait wait = new WebDriverWait(driver, specifiedTimeout);
+        ExpectedCondition<Boolean> urlIsCorrect = arg0 -> driver.getCurrentUrl().toLowerCase().equals(url.toLowerCase
+                ());
+        wait.until(urlIsCorrect);
+        waitForPageLoadComplete(driver, specifiedTimeout);
+    }
+
+    /**
+     * Wait for a URL containing a specified String to open in the browser, ignoring the case of the url.
+     * Checks whether a lower case value of the actual URL contains a lower case value of the expected String.
+     * Wait for the TIMEOUT number of seconds.
+     *
+     * @param expectedString - the String that needs to be included in the URL
+     * @param driver         - the WebDriver instance
+     */
+    public void waitForUrlContains_IgnoreCase(String expectedString, WebDriver driver) {
+        waitForUrlContains_IgnoreCase(expectedString, driver, TIMEOUT);
+    }
+
+    /**
+     * Wait for a URL containing a specified String to open in the browser, ignoring the case of the url.
+     * Checks whether a lower case value of the actual URL contains a lower case value of the expected String.
+     * Wait for the specifiedTimeout number of seconds.
+     *
+     * @param expectedString   - the String that needs to be included in the URL
+     * @param driver           - the WebDriver instance
+     * @param specifiedTimeout - amount of seconds you want to wait for
+     */
+    public void waitForUrlContains_IgnoreCase(String expectedString, WebDriver driver, int specifiedTimeout) {
+        WebDriverWait wait = new WebDriverWait(driver, specifiedTimeout);
+        ExpectedCondition<Boolean> urlIsCorrect = arg0 -> driver.getCurrentUrl().toLowerCase().contains(expectedString
+                .toLowerCase());
+        wait.until(urlIsCorrect);
+        waitForPageLoadComplete(driver, specifiedTimeout);
+    }
+
+    /**
+     * Wait for the URL in the browser to start with a specified String.
+     * Wait for the TIMEOUT number of seconds.
+     *
+     * @param expectedString - the expected String to be found at the start of the URL loaded in the browser
+     * @param driver         - the WebDriver instance
+     */
+    public void waitForUrlStartsWith(String expectedString, WebDriver driver) {
+        waitForUrlStartsWith(expectedString, driver, TIMEOUT);
+    }
+
+    /**
+     * Wait for the URL in the browser to start with a specified String.
+     * Wait for the specifiedTimeout number of seconds.
+     *
+     * @param expectedString   - the expected String to be found at the start of the URL loaded in the browser
+     * @param driver           - the WebDriver instance
+     * @param specifiedTimeout - amount of seconds you want to wait for
+     */
+    public void waitForUrlStartsWith(String expectedString, WebDriver driver, int specifiedTimeout) {
+        WebDriverWait wait = new WebDriverWait(driver, specifiedTimeout);
+        ExpectedCondition<Boolean> urlIsCorrect = arg0 -> driver.getCurrentUrl().startsWith(expectedString);
+        wait.until(urlIsCorrect);
+        waitForPageLoadComplete(driver, specifiedTimeout);
+    }
+
+    // CLICK AND WAIT METHODS
+
+    /**
+     * Click on a WebElement and than wait for a specified URL to load in the browser.
+     * Wait for TIMEOUT number of seconds.
+     *
+     * @param element - the WebElement to click on
+     * @param url     - the URL you need are waiting to load
+     * @param driver  - the WebDriver instance
+     */
+    public void clickElementAndWaitForUrl(WebElement element, String url, WebDriver driver) {
+        clickElementAndWaitForUrl(element, url, driver, TIMEOUT);
+    }
+
+    /**
+     * Click on a WebElement and than wait for a specified URL to load in the browser.
+     * Wait for the specifiedTimeout number of seconds.
+     *
+     * @param element          - the WebElement to click on
+     * @param url              - the URL you need are waiting to load
+     * @param driver           - the WebDriver instance
+     * @param specifiedTimeout - amount of seconds you want to wait for
+     */
+    public void clickElementAndWaitForUrl(WebElement element, String url, WebDriver driver, int specifiedTimeout) {
+        element.click();
+        waitForUrl(url, driver, specifiedTimeout);
+    }
+
+    /**
+     * Click on a WebElement and than wait for the URL that loads in the browser to contain a specified String.
+     * Wait for the TIMEOUT number of seconds.
+     *
+     * @param element             - the WebElement to click on
+     * @param expectedStringInUrl - the String you expect to be contained in the URL that loads after clicking the
+     *                            WebElement
+     * @param driver              - the WebDriver instance
+     */
+    public void clickElementAndWaitForUrlContains(WebElement element, String expectedStringInUrl, WebDriver driver) {
+        clickElementAndWaitForUrlContains(element, expectedStringInUrl, driver, TIMEOUT);
+
+    }
+
+    /**
+     * Click on a WebElement and than wait for the URL that loads in the browser to contain a specified String.
+     * Wait for the specifiedTimeout number of seconds.
+     *
+     * @param element             - the WebElement to click on
+     * @param expectedStringInUrl - the String you expect to be contained in the URL that loads after clicking the
+     *                            WebElement
+     * @param driver              - the WebDriver instance
+     * @param specifiedTimeout    - amount of seconds you want to wait for
+     */
+    public void clickElementAndWaitForUrlContains(WebElement element, String expectedStringInUrl, WebDriver driver, int
+            specifiedTimeout) {
+        element.click();
+        waitForUrlContains(expectedStringInUrl, driver, specifiedTimeout);
+    }
+
+    /**
+     * Click on a WebElement and than wait for a specified URL to load in the browser, ignoring the case.
+     * Wait for TIMEOUT number of seconds.
+     *
+     * @param element - the WebElement to click on
+     * @param url     - the URL you need are waiting to load
+     * @param driver  - the WebDriver instance
+     */
+    public void clickElementAndWaitForUrl_IgnoreCase(WebElement element, String url, WebDriver driver) {
+        clickElementAndWaitForUrl_IgnoreCase(element, url, driver, TIMEOUT);
+    }
+
+    /**
+     * Click on a WebElement and than wait for a specified URL to load in the browser, ignoring the case.
+     * Wait for the specifiedTimeout number of seconds.
+     *
+     * @param element          - the WebElement to click on
+     * @param url              - the URL you need are waiting to load
+     * @param driver           - the WebDriver instance
+     * @param specifiedTimeout - amount of seconds you want to wait for
+     */
+    public void clickElementAndWaitForUrl_IgnoreCase(WebElement element, String url, WebDriver driver, int
+            specifiedTimeout) {
+        element.click();
+        waitForUrl_IgnoreCase(url, driver, specifiedTimeout);
+    }
+
+    /**
+     * Click on a WebElement and than wait for the URL that loads in the browser to contain a specified String,
+     * ignoring the case.
+     * Wait for the TIMEOUT number of seconds.
+     *
+     * @param element             - the WebElement to click on
+     * @param expectedStringInUrl - the String you expect to be contained in the URL that loads after clicking the
+     *                            WebElement
+     * @param driver              - the WebDriver instance
+     */
+    public void clickElementAndWaitForUrlContains_IgnoreCase(WebElement element, String expectedStringInUrl, WebDriver
+            driver) {
+        clickElementAndWaitForUrlContains_IgnoreCase(element, expectedStringInUrl, driver, TIMEOUT);
+
+    }
+
+    /**
+     * Click on a WebElement and than wait for the URL that loads in the browser to contain a specified String,
+     * ignoring the case.
+     * Wait for the specifiedTimeout number of seconds.
+     *
+     * @param element             - the WebElement to click on
+     * @param expectedStringInUrl - the String you expect to be contained in the URL that loads after clicking the
+     *                            WebElement
+     * @param driver              - the WebDriver instance
+     * @param specifiedTimeout    - amount of seconds you want to wait for
+     */
+    public void clickElementAndWaitForUrlContains_IgnoreCase(WebElement element, String expectedStringInUrl, WebDriver
+            driver, int specifiedTimeout) {
+        element.click();
+        waitForUrlContains_IgnoreCase(expectedStringInUrl, driver, specifiedTimeout);
+    }
+
+    /**
+     * Click on a WebElement and wait for the URL in the browser to start with an expected String.
+     * Wait for the TIMEOUT number of seconds.
+     *
+     * @param element          - the WebElement to click on
+     * @param expectedString   - the String you expected the URL in the browser to start with
+     * @param driver           - the WebDriver instance
+     */
+    public void clickElementAndWaitForUrlStartsWith(WebElement element, String expectedString, WebDriver
+            driver) {
+        clickElementAndWaitForUrlStartsWith(element, expectedString, driver, TIMEOUT);
+    }
+
+    /**
+     * Click on a WebElement and wait for the URL in the browser to start with an expected String.
+     * Wait for the specifiedTimeout number of seconds.
+     *
+     * @param element          - the WebElement to click on
+     * @param expectedString   - the String you expected the URL in the browser to start with
+     * @param driver           - the WebDriver instance
+     * @param specifiedTimeout - amount of seconds you want to wait for
+     */
+    public void clickElementAndWaitForUrlStartsWith(WebElement element, String expectedString, WebDriver
+            driver, int specifiedTimeout) {
+        element.click();
+        waitForUrlStartsWith(expectedString, driver, specifiedTimeout);
+    }
+
 }
